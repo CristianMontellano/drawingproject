f35f45d0f439bb3639c6d5b8c5014bffcf923742
Cristian Montellano <cristianmontellano32@gmail.com>
2016-10-11 09:18:49 -0700

    Drawing Project

---
+++ drawingproject.js
@@ -0,0 +1,47 @@
+var canvasDiv = document.getElementById("drawingArea");
+var canvas = canvasDiv.getContext('2d');
+
+//Hat Layer Top
+canvas.fillStyle = "red";
+canvas.fillRect(200, 50, 50, 50);
+
+canvas.fillStyle = "red";
+canvas.fillRect(250, 50, 50, 50);
+
+canvas.fillStyle = "red";
+canvas.fillRect(300, 50, 50, 50);
+
+//Hat Layer Bottom
+canvas.fillStyle = "red";
+canvas.fillRect(150, 100, 50, 50);
+
+canvas.fillStyle = "red";
+canvas.fillRect(200, 100, 50, 50);
+
+canvas.fillStyle = "red";
+canvas.fillRect(250, 100, 50, 50);
+
+canvas.fillStyle = "red";
+canvas.fillRect(300, 100, 50, 50);
+
+canvas.fillStyle = "red";
+canvas.fillRect(350, 100, 50, 50);
+
+canvas.fillStyle = "red";
+canvas.fillRect(400, 100, 50, 50);
+
+//Head
+canvas.fillStyle = "brown";
+canvas.fillRect(150, 150, 150, 50);
+
+canvas.fillStyle = "brown";
+canvas.fillRect(200, 200, 50, 100);
+
+canvas.fillStyle = "brown";
+canvas.fillRect(250, 250, 50, 50);
+
+canvas.fillStyle = "#FFAD60";
+canvas.fillRect(150, 200, 50, 100);
+
+canvas.fillStyle = "brown";
+canvas.fillRect(100, 200, 50, 150);
